c75fea0a96e21a57cbe4e3917823ff97274d286f
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-26 20:48:05 -0400

    dntknow

--- src/app.js
+++ src/app.js
@@ -51,6 +51,7 @@ App = {
   loadAccount: async () => {
     // Set the current blockchain account
     App.account = web3.eth.accounts[0];
+    console.log(App.account);
   },
 
   loadContract: async () => {
